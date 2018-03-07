d44af93a956f7c8c78f7d11ad8aea99386e737ef
Matt <faymatt96@gmail.com>
2018-03-07 17:49:53 -0500

    File changes

---
+++ chap3.js
@@ -0,0 +1,23 @@
+/* Lists */
+// 1. Especially useful if we don't have to perform searches on the items in the list or put
+// them into some type of sorted order.
+// 2. When we need to perform long searches or complex sorts, lists become less useful,
+// especially with more complex data structures.
+
+// List Abstract Data Type(ADT):
+
+// listSize (property) - Number of elements in list
+// pos (property) - Current position in list
+// length (property) - Returns the number of elements in list
+// clear (function) - Clears all elements from list
+// toString (function) - Returns string representation of list
+// getElement (function) - Returns element at current position
+// insert (function) - Inserts new element after existing element
+// append (function) - Adds new element to end of list
+// remove (function) - Removes element from list
+// front (function) - Sets current position to first element of list
+// end (function) - Sets current position to last element of list
+// prev (function) - Moves current position back one element
+// next (function) - Moves current position forward one element
+// currPos (function) - Returns the current position in list
+// moveTo (function) - Moves the current position to specified position
